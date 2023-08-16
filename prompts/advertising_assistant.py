91ffc43d97c78f18bf653e7d38ee21a27e196f66
jaredkirby <jpeterkirby@gmail.com>
2023-08-16 13:01:31 -0700

    cleanup

---
+++ prompts/advertising_assistant.py
@@ -0,0 +1,117 @@
+ADVERTISING_ASSISTANT_JOB_DESCRIPTION = """
+Summary of Position:
+This in-house position is crucial for supporting our client's activity. The Advertising Assistant works closely with the account teams to execute both marketing and media campaigns across numerous clients. This individual will need to be a go-getter who can juggle multiple tasks, priorities, and programs. Excellent junior-level position with a fast-paced national advertising agency to learn and grow/start your marketing career.
+
+Core Duties/Responsibilities:
+Marketing and media research
+Program execution support
+Vendor communication
+Project file management
+Creative trafficking and support
+Reporting
+Data Entry
+Light Office Duties
+
+Experience:
+Proficiency in Microsoft Office Suite (Excel, Word, PowerPoint)
+Professionalism, proactive attitude, and meticulous attention to detail
+Strong verbal and written skills
+Ability to work independently and as a team player
+Multitasking and handling multiple projects
+Positive energy and enthusiasm
+Willingness to ask questions and learn
+Strong interpersonal and communication skills
+"""
+
+ADVERTISING_ASSISTANT_HIGH_FIT_RESUME = """
+Example "high-fit" resume with a score of 0.9 for reference:
+-----------------
+Example "High-Fit" Resume for Advertising Assistant Position
+Name: [Candidate Name]
+Address: [Address]
+Phone: [Phone Number]
+Email: [Email Address]
+
+Professional Summary
+Motivated marketing professional with over 3 years of experience in brand advertisement, event management, and direct marketing. Proficient in Microsoft Office Suite, Adobe Creative Suite, and skilled in building customer relationships, understanding market trends, and executing marketing campaigns. Bilingual in English and Spanish, with strong attention to detail and ability to work independently and as a team player.
+
+Education
+Bachelor of Science, Business Administration
+Concentration in Marketing & Advertising Emphasis
+California State University Dominguez Hills
+Work Experience
+Assistant Marketing Manager, GameStop (2017 - 2023)
+
+Assisted in the development and execution of marketing campaigns, including social media, email, and digital advertising.
+Conducted market research to identify customer trends and competitor offerings.
+Managed project timelines and budgets; coordinated with external vendors.
+Analyzed marketing campaign performance data; provided insights to senior management.
+Marketing & Promotions Manager, Baja Sharkeez (03/2023 - 06/2023)
+
+Conferred with department heads to plan advertising and promotional material.
+Managed team goals, incentives, and weekly performance evaluations.
+Coordinated company-wide events, including tradeshows and conferences.
+Events Management Intern, Downtown SLO (2022)
+
+Facilitated weekly community events, including vendor coordination and promotion of local businesses.
+Operated Concerts in the Plaza series; maintained artist and community relations.
+Sales Associate, Tillys (2022 - 2023)
+
+Assisted guests with inquiries; coordinated direct-to-customer online pickup orders.
+Handled POS system; maintained knowledge of product offerings and ongoing deals.
+Skills
+Marketing Skills: Direct and indirect marketing, social media platforms, content creation.
+Communication Skills: Excellent verbal and written communication, bilingual in English and Spanish.
+Technical Skills: Proficient in Microsoft Office Suite, Excel, Word, PowerPoint, Adobe Creative Cloud, Photoshop.
+Interpersonal Skills: Strong attention to detail, ability to work independently and as part of a team, multitasking.
+References
+Available upon request.
+-----------------
+"""
+
+ADVERTISING_ASSISTANT_LOW_FIT_RESUME = """
+Example "low-fit" resume with a score of 0.1 for reference:
+-----------------
+Example "Low-Fit" Resume for Advertising Assistant Position
+Name: [Candidate Name]
+Address: [Address]
+Phone: [Phone Number]
+Email: [Email Address]
+
+Professional Summary
+Highly experienced marine biologist with a focus on oceanographic research and marine conservation. Over 10 years of experience in conducting field studies, analyzing aquatic ecosystems, and publishing scientific papers. Limited experience in marketing or advertising.
+
+Education
+Ph.D. in Marine Biology
+Ocean University
+
+Master of Science in Marine Biology
+Sea Research Institute
+
+Bachelor of Science in Biology
+Coastal College
+
+Work Experience
+Senior Marine Biologist, Ocean Exploration Center (2010 - Present)
+
+Conducted deep-sea research expeditions.
+Analyzed aquatic ecosystems and marine life behavior.
+Published multiple scientific papers in peer-reviewed journals.
+Aquarium Curator, Marine Life Museum (2005 - 2010)
+
+Managed exhibits and cared for various marine species.
+Led educational tours and workshops for visitors.
+Collaborated with conservation organizations on environmental initiatives.
+Field Research Assistant, Coastal Research Lab (2000 - 2005)
+
+Assisted in collecting and analyzing oceanographic data.
+Supported research projects related to marine ecology and climate change.
+Skills
+Scientific Skills: Research design, data analysis, scientific writing, fieldwork.
+Technical Skills: Proficient in statistical software, GIS mapping, lab equipment.
+Environmental Skills: Marine conservation, ecological assessment, environmental education.
+Other Skills: Basic knowledge of Microsoft Word (No experience with Excel or PowerPoint).
+References
+Available upon request.
+-----------------
+"""
