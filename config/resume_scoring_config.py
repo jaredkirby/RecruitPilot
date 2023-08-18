9e1df058c4e9887d92c3e01522c191f04439955b
jaredkirby <jpeterkirby@gmail.com>
2023-08-18 06:55:22 -0700

    Updates

---
+++ config/resume_scoring_config.py
@@ -0,0 +1,22 @@
+# LLM
+MODEL = "gpt-3.5-turbo-16k"
+
+# Tool Name
+SUB_TITLE = "📈 Resume Scoring"
+
+# How to use
+INSTRUCTIONS = """
+This tool 🛠️ uses the OpenAI API 🧠 to score (with an explanation) and categorize resumes based on a job description Then provides a downloadable file with the original resumes in their respective categories.
+
+1️⃣) Select a job description from the dropdown menu or input your own ✏️.
+
+1️⃣ .5) If you input your own job description, you can also upload a PDF or enter text examples of a high-fit ✅ and low-fit ❌ resume to the specified job description. This will increase the accuracy 🎯 of the scoring.
+
+2️⃣) Upload resumes to score and categorize 📑. You can upload multiple resumes at once.
+
+3️⃣) Click the "Start Scoring Resumes" button.
+
+4️⃣) Once the process is complete, a download button will appear to export the scores and categorized resumes 📥.
+
+📌Note: The OpenAI models do make mistakes 😅 and the results may not be perfect ✨. If you have any questions or feedback, please reach out to me on [Twitter](https://twitter.com/Kirby_).
+"""
---
+++ config/site_config.py
@@ -0,0 +1,11 @@
+# All Page Config
+LAYOUT = "centered"
+PAGE_TITLE = "RecruitPilot"
+PAGE_ICON = "🎯"
+FOOTER = """
+    ---
+    Built by **Jared Kirby** :wave:
+
+    [Twitter](https://twitter.com/Kirby_) | [GitHub](https://github.com/jaredkirby) | [LinkedIn](https://www.linkedin.com/in/jared-kirby/) | [Portfolio](https://www.jaredkirby.me)
+
+        """
